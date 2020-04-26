798115e72514ffb8d1aa6b6d4e821bc52eaec235
rajesh <kadam.raj1001@outlook.com>
2020-04-26 23:08:16 +0530

    winmobile

---
+++ WinFormMobile/Mobile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+
+namespace WinFormMobile
+{
+    public partial class Mobile : Form
+    {
+        public Mobile()
+        {
+            InitializeComponent();
+        }
+
+        private void Mobile_Load(object sender, EventArgs e)
+        {
+            PopulateDataGridView();
+        }
+
+        private void PopulateDataGridView()
+        {
+            string apiUrl = "https://apimobiledetails.azurewebsites.net/api/mobile";
+            object input = new
+            {
+                CompanyName = ""//OnePlus
+            };
+            string inputJson = (new JavaScriptSerializer()).Serialize(input);
+            System.Net.WebClient client = new WebClient();
+            client.Headers["Content-type"] = "application/json";
+            client.Encoding = Encoding.UTF8;
+            
+            string json = client.UploadString(apiUrl  + "/GetListMobile/", inputJson);
+
+            dataGridView1.DataSource = (new JavaScriptSerializer()).Deserialize<List<MobileModel>>(json);
+        }
+    }
+}
---
+++ WinFormMobile/MobileModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormMobile
+{
+    class MobileModel
+    {
+
+
+        public int MobileID { get; set; }
+        public string MobileName { get; set; }
+        public string CompanyName { get; set; }
+        public string Price { get; set; }
+
+    }
+}
+
